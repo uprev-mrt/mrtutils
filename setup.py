d93a865c4463bf0b69b4523fa4b770ab1da6818b
Jason Berger <berge472@gmail.com>
2020-02-02 11:29:31 -0500

    added template example

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='mrtutils',
-     version='0.0.07',
+     version='0.1.08',
      author="Jason Berger",
      author_email="JBerger@up-rev.com",
      description="Utilities for MrT",
