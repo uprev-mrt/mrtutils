b83c775471aaf4bf35184ad3154ed46b5504082b
Jason Berger <JBerger@up-rev.com>
2021-04-19 00:01:39 -0400

    saving

--- mrtutils/modsync.py
+++ mrtutils/modsync.py
@@ -257,6 +257,8 @@ class Repo:
             self.dir.add(mod, 0)
 
     def getReadMe(self):
+
+        data = ""
         if self.isRemote:
             if(self.isBitbucket):
                 nodes = self.url.split('/')
--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ import setuptools
 
 
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
      name='mrtutils',
-     version='0.1.85',
+     version='0.1.87',
      author="Jason Berger",
      author_email="JBerger@up-rev.com",
      description="Utilities for MrT",
      long_description=long_description,
      scripts=['mrtutils/mrt-config','mrtutils/mrt-device', 'mrtutils/mrt-ble', 'mrtutils/mrt-config-gui'],
-     long_description_content_type="text/markdown",
+     long_description_content_type="text/x-rst",
      url="http://www.up-rev.com/",
      packages=setuptools.find_packages(),
      package_data={
